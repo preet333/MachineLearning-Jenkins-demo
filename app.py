05c4afa9c40598c4b79bc295012682dd4aa5df96
preet333 <preetpatel3398@gmail.com>
2023-05-03 17:32:40 +0530

    flask app added

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import joblib
+
+model = joblib.load("model/model.pkl")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    if request.method == 'POST':
+        square_foot = int(request.form['squarefoot'])
+        room = int(request.form['room'])
+        predict = model.predict([[square_foot, room]])
+    return render_template('index.html', prediction=predict)
+
+if __name__ == '__main__':
+    app.run(debug=True)    
